e55b3382446fc7aa3b7ad726ddda9e96e225d73c
Ryo Asai <fu.orangina0516@gmail.com>
2021-10-20 22:33:34 +0900

    string型

--- main.go
+++ main.go
@@ -102,3 +102,24 @@ func outer() {
 
     
 // }
+
+func main() {
+    var s string = "hello golang"
+    fmt.Println(s)
+
+    fmt.Printf("%T\n", s)
+
+    var si string = "300"
+    fmt.Println(si)
+
+    fmt.Println(`test
+    test
+        test
+    `)
+
+    fmt.Println("\"")
+    fmt.Println(`""`)
+
+    fmt.Println(s[0]) //72 byte型
+    fmt.Println(string(s[0])) //h
+}
