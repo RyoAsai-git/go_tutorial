ea2d90183973b1141c09bfd75912cbe5d7896701
Ryo Asai <fu.orangina0516@gmail.com>
2021-10-20 22:17:54 +0900

    int型

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
     "fmt"
-    "time"
+    // "time"
 )
 
 var i5 int = 500
@@ -12,46 +12,60 @@ func outer() {
     fmt.Println(s4)
 }
 
-func main() {
-    fmt.Println("Hello World")
-    fmt.Println(time.Now())
+// func main() {
+//     fmt.Println("Hello World")
+//     fmt.Println(time.Now())
 
+//     var i int = 100
+//     fmt.Println(i)
+
+//     var s string = "Hello Go"
+//     fmt.Println(s)
+
+//     var t,f bool = true, false
+//     fmt.Println(t, f)
+
+//     var (
+//         i2 int = 200
+//         s2 string = "Golang"
+//     )
+//     fmt.Println(i2, s2)
+
+//     var i3 int 
+//     var s3 string
+//     fmt.Println(i3, s3) //0 //空文字
+
+//     i3 = 300
+//     s3 = "Go"
+//     fmt.Println(i3, s3)
+
+//     i = 150 //値の更新
+//     fmt.Println(i)
+
+//     //暗黙的な定義
+//     i4 := 400
+//     fmt.Println(i4)
+
+//     i4 = 450
+//     fmt.Println(i4)
+
+
+//     fmt.Println(i5)
+
+//     outer()
+
+// }
+
+
+func main () {
     var i int = 100
-    fmt.Println(i)
 
-    var s string = "Hello Go"
-    fmt.Println(s)
+    var i64 int64 = 200
 
-    var t,f bool = true, false
-    fmt.Println(t, f)
+    fmt.Println(i + 100)
+    
+    fmt.Printf("%T\n", i64) //int64
 
-    var (
-        i2 int = 200
-        s2 string = "Golang"
-    )
-    fmt.Println(i2, s2)
-
-    var i3 int 
-    var s3 string
-    fmt.Println(i3, s3) //0 //空文字
-
-    i3 = 300
-    s3 = "Go"
-    fmt.Println(i3, s3)
-
-    i = 150 //値の更新
-    fmt.Println(i)
-
-    //暗黙的な定義
-    i4 := 400
-    fmt.Println(i4)
-
-    i4 = 450
-    fmt.Println(i4)
-
-
-    fmt.Println(i5)
-
-    outer()
+    fmt.Printf("%T\n", int32(i64)) //変換
+    //型が異なると計算ができない
 }
-
