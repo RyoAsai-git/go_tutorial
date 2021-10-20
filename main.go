a9a8abb412ac306ffd1b8360180a3d03e95173ad
Ryo Asai <fu.orangina0516@gmail.com>
2021-10-20 22:29:27 +0900

    bool型

--- main.go
+++ main.go
@@ -71,27 +71,34 @@ func outer() {
 // }
 
 
-func main() {
-    var fl64 float64 = 2.4
-    fmt.Println(fl64)
+// func main() {
+//     var fl64 float64 = 2.4
+//     fmt.Println(fl64)
 
-    fl := 3.2
-    fmt.Println(fl64 + fl)
-    fmt.Printf("%T", "%T\n", fl64, fl)
+//     fl := 3.2
+//     fmt.Println(fl64 + fl)
+//     fmt.Printf("%T", "%T\n", fl64, fl)
 
-    var fl32 float32 = 1.2
-    fmt.Printf("%T\n", fl32)
+//     var fl32 float32 = 1.2
+//     fmt.Printf("%T\n", fl32)
 
-    fmt.Printf("%T\n", float64(fl32))
+//     fmt.Printf("%T\n", float64(fl32))
 
-    zero := 0.0
-    pinf := 1.0 / zero
-    fmt.Println(pinf)
+//     zero := 0.0
+//     pinf := 1.0 / zero
+//     fmt.Println(pinf)
 
-    ninf := -1.0 / zero
-    fmt.Println(ninf)
+//     ninf := -1.0 / zero
+//     fmt.Println(ninf)
 
-    nan := zero / zero
-    fmt.Println(nan)
+//     nan := zero / zero
+//     fmt.Println(nan)
 
-}
+// }
+
+// func main() {
+//     var t, f bool = true, false
+//     fmt.Println(t, f)
+
+    
+// }
