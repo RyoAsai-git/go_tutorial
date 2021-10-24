617bc77e079bc01be3b998af3d7e706667acd1c2
Ryo Asai <fu.orangina0516@iCloud.com>
2021-10-24 16:16:27 +0900

    append make cap

--- main.go
+++ main.go
@@ -443,36 +443,63 @@ import (
 
 //スライス
 
-func main() {
-    var sl []int
-    fmt.Println(sl)
+// func main() {
+//     var sl []int
+//     fmt.Println(sl)
 
-    var sl2 []int = []int{100, 200}
-    fmt.Println(sl2)
+//     var sl2 []int = []int{100, 200}
+//     fmt.Println(sl2)
 
-    sl3 := []string{"A", "B"}
-    fmt.Println(sl3)
+//     sl3 := []string{"A", "B"}
+//     fmt.Println(sl3)
 
-    sl4 := make([]int, 5)
-    fmt.Println(sl4)
+//     sl4 := make([]int, 5)
+//     fmt.Println(sl4)
 
-    sl2[0] = 1000
-    fmt.Println(sl2)
+//     sl2[0] = 1000
+//     fmt.Println(sl2)
 
-    sl5 := []int{1, 2, 3, 4, 5}
-    fmt.Println(sl5)
+//     sl5 := []int{1, 2, 3, 4, 5}
+//     fmt.Println(sl5)
 
-    fmt.Println(sl5[0])
+//     fmt.Println(sl5[0])
 
-    fmt.Println(sl5[2:4])
+//     fmt.Println(sl5[2:4])
 
-    fmt.Println(sl5[:2])
+//     fmt.Println(sl5[:2])
 
-    fmt.Println(sl5[2:])
+//     fmt.Println(sl5[2:])
 
-    fmt.Println(sl5[:])
+//     fmt.Println(sl5[:])
 
-    fmt.Println(sl5[len(sl5) - 1])
+//     fmt.Println(sl5[len(sl5) - 1])
     
-    fmt.Println(sl5[1:len(sl5) -1])
-}
+//     fmt.Println(sl5[1:len(sl5) -1])
+// }
+
+// func main() {
+//     sl := []int{100, 200}
+//     fmt.Println(sl)
+
+//     sl = append(sl, 300)
+//     fmt.Println(sl)
+
+//     sl = append(sl, 400, 500, 600)
+//     fmt.Println(sl)
+
+//     sl2 := make([]int, 5)
+//     fmt.Println(sl2)
+
+//     fmt.Println(len(sl2))
+
+//     fmt.Println(cap(sl2))
+
+//     sl3 := make([]int, 5, 10)
+
+//     fmt.Println(len(sl3))
+//     fmt.Println(cap(sl3))
+
+//     sl3 = append(sl3, 1, 2, 3, 4, 5, 6, 7)
+//     fmt.Println(len(sl3))
+//     fmt.Println(cap(sl3))
+// }
