6900c80fe87a86935f289b6d46425582a5ee5b32
Ryo Asai <fu.orangina0516@iCloud.com>
2021-10-24 21:30:09 +0900

    チャネルのfor文

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
     "fmt"
-    "time"
+    // "time"
     // "strconv"
     // "os"
     // "time"
@@ -700,41 +700,52 @@ import (
 //     }
 // }
 
-func receiver(name string, ch chan int) {
-    for {
-        i, ok := <-ch
-        if !ok {
-            break
-        }
-        fmt.Println(name, i)
-    }
-    fmt.Println(name + "END")
-}
+// func receiver(name string, ch chan int) {
+//     for {
+//         i, ok := <-ch
+//         if !ok {
+//             break
+//         }
+//         fmt.Println(name, i)
+//     }
+//     fmt.Println(name + "END")
+// }
+
+// func main() {
+//     ch1 := make(chan int, 2)
+
+//     // ch1 <- 1
+//     // close(ch1)
+
+//     // // ch1 <- 1
+//     // // fmt.Println(<-ch1)
+
+//     // i, ok := <- ch1
+//     // fmt.Println(i, ok)
+
+//     // i2, ok := <- ch1
+//     // fmt.Println(i2, ok)
+
+//     go receiver("1.goroutine", ch1)
+//     go receiver("2.goroutine", ch1)
+//     go receiver("3.goroutine", ch1)
+
+//     i := 0
+//     for i < 100 {
+//         ch1 <- i
+//         i++
+//     }
+//     close(ch1)
+//     time.Sleep(3 * time.Millisecond)
+// }
 
 func main() {
-    ch1 := make(chan int, 2)
-
-    // ch1 <- 1
-    // close(ch1)
-
-    // // ch1 <- 1
-    // // fmt.Println(<-ch1)
-
-    // i, ok := <- ch1
-    // fmt.Println(i, ok)
-
-    // i2, ok := <- ch1
-    // fmt.Println(i2, ok)
-
-    go receiver("1.goroutine", ch1)
-    go receiver("2.goroutine", ch1)
-    go receiver("3.goroutine", ch1)
-
-    i := 0
-    for i < 100 {
-        ch1 <- i
-        i++
-    }
+    ch1 := make(chan int, 3)
+    ch1 <- 1
+    ch1 <- 2
+    ch1 <- 3
     close(ch1)
-    time.Sleep(3 * time.Millisecond)
+    for i := range ch1 {
+        fmt.Println(i)
+    }
 }
