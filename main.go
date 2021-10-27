fad17503bc666dc9a06203ad941c9abcc49e91d6
Ryo Asai <fu.orangina0516@iCloud.com>
2021-10-27 22:58:02 +0900

    POST

--- main.go
+++ main.go
@@ -34,68 +34,146 @@
 //     // => "9989a8fcbc29044b5883a0a36c146fe7415b1439e995b4d806ea0af7da9ca4390eb92@<dtp>{lb}a604b3ecfa3d75f9911c768fbe2aecc59eff1e48dcaeca1957bdde01dfb"
 // }
 
+// package main
+ 
+// import (
+// 	"fmt"
+// 	"io/ioutil"
+// 	"net/http"
+// 	"net/url"
+// )
+ 
+// //Get
+ 
+// func main() {
+// 	//応用
+// 	//ヘッダーをつけたり、クエリをつけたり
+// 	//Parse 正しいURLか確認
+// 	base, _ := url.Parse("https://example.com/")
+ 
+// 	//クエリ の確認 URLの後につく
+// 	reference, _ := url.Parse("index/lists?id=1")
+ 
+// 	//ResolveReference
+// 	//クエリをくっつけたURLを生成する。
+// 	//相対パスから絶対URLに変換する。
+// 	//baseのURLの末尾に文字列が入っていたとしても、正しいURLに直してくれる
+// 	endpoint := base.ResolveReference(reference).String()
+// 	fmt.Println(endpoint)
+ 
+// 	//GET ver
+// 	//リクエストを作成 nil部はPOST時のみ設定（バイトを入れる）
+// 	//まだリクエストはしていない。structを作っただけ。
+// 	req, _ := http.NewRequest("GET", endpoint, nil)
+ 
+// 	//requestにheaderをつける。cash情報など
+// 	req.Header.Add("Content-Type", `application/json"`)
+ 
+// 	//URLのクエリを確認
+// 	q := req.URL.Query()
+ 
+// 	//クエリを追加
+// 	q.Add("name", "test")
+ 
+// 	//クエリを表示
+// 	fmt.Println(q)
+ 
+// 	//&など特殊文字などがある場合があるため、encodingしてからURLに追加してやる必要がある
+// 	fmt.Println(q.Encode())
+ 
+// 	//encodeしてからURLに戻す
+// 	//日本語などを変換する
+// 	req.URL.RawQuery = q.Encode()
+ 
+// 	//実際にアクセスする
+// 	//クライアントを作る
+// 	var client *http.Client = &http.Client{}
+ 
+// 	//結果 アクセスする
+// 	resp, _ := client.Do(req)
+ 
+// 	//読み込み
+// 	body, _ := ioutil.ReadAll(resp.Body)
+ 
+// 	//出力
+// 	fmt.Println(string(body))
+ 
+// }
+
 package main
  
 import (
-	"fmt"
-	"io/ioutil"
-	"net/http"
-	"net/url"
+    "bytes"
+    "encoding/json"
+    "fmt"
+    "io/ioutil"
+    "net/http"
+    "net/url"
 )
  
-//Get
+//応用
+type Account struct {
+    ID       string
+    PassWord string
+}
+ 
+//Post
  
 func main() {
-	//応用
-	//ヘッダーをつけたり、クエリをつけたり
-	//Parse 正しいURLか確認
-	base, _ := url.Parse("https://example.com/")
+    //応用
+    //ヘッダーをつけたり、クエリをつけたり
+    //Parse 正しいURLか確認
+    base, _ := url.Parse("https://example.com/")
  
-	//クエリ の確認 URLの後につく
-	reference, _ := url.Parse("index/lists?id=1")
+    //クエリ の確認 URLの後につく
+    reference, _ := url.Parse("index/lists?id=1")
  
-	//ResolveReference
-	//クエリをくっつけたURLを生成する。
-	//相対パスから絶対URLに変換する。
-	//baseのURLの末尾に文字列が入っていたとしても、正しいURLに直してくれる
-	endpoint := base.ResolveReference(reference).String()
-	fmt.Println(endpoint)
+    //Postの時のデータ
+    AccountData := &Account{ID: "ABC-DEF", PassWord: "testtest"}
+    data, _ := json.Marshal(AccountData)
  
-	//GET ver
-	//リクエストを作成 nil部はPOST時のみ設定（バイトを入れる）
-	//まだリクエストはしていない。structを作っただけ。
-	req, _ := http.NewRequest("GET", endpoint, nil)
+    //ResolveReference
+    //クエリをくっつけたURLを生成する。
+    //相対パスから絶対URLに変換する。
+    //baseのURLの末尾に文字列が入っていたとしても、正しいURLに直してくれる
+    endpoint := base.ResolveReference(reference).String()
+    fmt.Println(endpoint)
  
-	//requestにheaderをつける。cash情報など
-	req.Header.Add("Content-Type", `application/json"`)
+    //POST ver
+    //bytes.NewBuffer([]byte("password")でリクエストの領域を作成
+    //POSTの場合は、Bodyにデータを入れる。例えばパスワード。見られたらダメな情報はbodyに
+    req, _ := http.NewRequest("POST", endpoint, bytes.NewBuffer(data))
  
-	//URLのクエリを確認
-	q := req.URL.Query()
+    //requestにheaderをつける。cash情報など
+    req.Header.Add("Content-Type", `application/json"`)
  
-	//クエリを追加
-	q.Add("name", "test")
+    //URLのクエリを確認
+    q := req.URL.Query()
  
-	//クエリを表示
-	fmt.Println(q)
+    //クエリを追加
+    q.Add("name", "test")
  
-	//&など特殊文字などがある場合があるため、encodingしてからURLに追加してやる必要がある
-	fmt.Println(q.Encode())
+    //クエリを表示
+    fmt.Println(q)
  
-	//encodeしてからURLに戻す
-	//日本語などを変換する
-	req.URL.RawQuery = q.Encode()
+    //&など特殊文字などがある場合があるため、encodingしてからURLに追加してやる必要がある
+    fmt.Println(q.Encode())
  
-	//実際にアクセスする
-	//クライアントを作る
-	var client *http.Client = &http.Client{}
+    //encodeしてからURLに戻す
+    //日本語などを変換する
+    req.URL.RawQuery = q.Encode()
  
-	//結果 アクセスする
-	resp, _ := client.Do(req)
+    //実際にアクセスする
+    //クライアントを作る
+    var client *http.Client = &http.Client{}
  
-	//読み込み
-	body, _ := ioutil.ReadAll(resp.Body)
+    //結果 アクセスする
+    resp, _ := client.Do(req)
  
-	//出力
-	fmt.Println(string(body))
+    //読み込み
+    body, _ := ioutil.ReadAll(resp.Body)
+ 
+    //出力
+    fmt.Println(string(body))
  
 }
