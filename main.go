1f3e4dc6275cea45f26a4531007aabf41da1aa8d
Ryo Asai <fu.orangina0516@iCloud.com>
2021-11-01 22:25:25 +0900

    iniファイル main実行前に読み込み

--- main.go
+++ main.go
@@ -1,33 +1,68 @@
+// package main
+
+// import (
+//     "database/sql"
+//     "log"
+//     _ "github.com/lib/pq"
+
+// )
+
+// var Db *sql.DB
+
+// var err error
+
+// type Person struct {
+//     Name string
+//     Age int
+// }
+
+// func main() {
+//     //userとdbをここで設定
+//     Db, err := sql.Open("postgres", "user=test_user dbname=testdb password=password sslmode=disable")
+//     if err != nil {
+//         log.Println(err)
+//     }
+//     defer Db.Close()
+
+//     cmd := "INSERT INTO persons (name, age) VALUES ($1, $2)"
+//     _, err = Db.Exec(cmd, "Nancy", 20)
+
+//     if err != nil {
+//         log.Fatalln(err)
+//     }
+// }
+
 package main
 
 import (
-    "database/sql"
-    "log"
-    _ "github.com/lib/pq"
+    "fmt"
 
+    "gopkg.in/go-ini/ini.v1"
 )
 
-var Db *sql.DB
+type ConfigLite struct {
+    Port      int
+    DbName    string
+    SQLDriver string
+}
 
-var err error
+var Config ConfigLite
 
-type Person struct {
-    Name string
-    Age int
+func init() {
+    cfg, _ := ini.Load("config.ini")
+
+    Config = ConfigLite{
+        //MustIntは数値型を読み込む 引数は初期値
+        Port: cfg.Section("web").Key("port").MustInt(8080),
+
+        DbName: cfg.Section("db").Key("name").MustString("example.sql"),
+
+        SQLDriver: cfg.Section("db").Key("driver").String(),
+    }
 }
 
 func main() {
-    //userとdbをここで設定
-    Db, err := sql.Open("postgres", "user=test_user dbname=testdb password=password sslmode=disable")
-    if err != nil {
-        log.Println(err)
-    }
-    defer Db.Close()
-
-    cmd := "INSERT INTO persons (name, age) VALUES ($1, $2)"
-    _, err = Db.Exec(cmd, "Nancy", 20)
-
-    if err != nil {
-        log.Fatalln(err)
-    }
+    fmt.Println("Port = %v\n", Config.Port)
+    fmt.Println("DbName = %v\n", Config.DbName, Config.DbName)
+    fmt.Println("SQLDriver = %v\n", Config.SQLDriver)
 }
